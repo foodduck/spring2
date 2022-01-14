7dd5fd7caf2837124b6f1e704b1bae00d814b355
leepd09 <leepd09@me.com>
2022-01-14 18:27:37 +0900

    0114

--- web04/src/main/java/kr/co/controller/BoardController.java
+++ web04/src/main/java/kr/co/controller/BoardController.java
@@ -1,39 +1,70 @@
 package kr.co.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class BoardController
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import kr.co.service.BoardService;
+import kr.co.service.ReplyService;
+import kr.co.vo.BoardVO;
+import kr.co.vo.PageMaker;
+import kr.co.vo.SearchCriteria;
+
+@Controller
+@RequestMapping("/board/*")
 public class BoardController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	//해당 내용을 처리하고, 콘솔 창에 메시지를 띄운다.
+	private final static Logger logger = LoggerFactory.getLogger(BoardController.class);
+   
+	@Inject
+	private BoardService service;
+	
+	@Inject
+	private ReplyService replyService;
+	
+	//게시판 글작성 화면으로 이동
+	@RequestMapping(value="/boardWriteView", method=RequestMethod.GET)
+	public void boardWriteView() throws Exception {
+		logger.info("boardWriteView");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+	//게시판 글쓰기 처리
+	@RequestMapping(value="/boardInsert", method=RequestMethod.POST)
+	public String boardInsert(BoardVO boardVO, MultipartHttpServletRequest mpRequest) throws Exception {
+		logger.info("boardInsert");
+		service.boardInsert(boardVO, mpRequest);
+		return "redirect:/bord/boardList";	//Dispatcher, forward, sendredirect 모두 합쳐진 형태
+		//데이터까지 같이 넘기게 된다.
+		//return "board/list";	데이터를 넘기지 않는다.
 	}
-
+	
+	//게시판 글 목록		
+	@RequestMapping(value="/boardList", method=RequestMethod.GET)
+	public String list(Model model, @ModelAttribute("scri") SearchCriteria scri) throws Exception {
+		logger.info("boardlist");
+		//model객체: getter가 필요없는 application 객체
+		model.addAttribute("boardlist", service.boardList(scri));
+		PageMaker pageMaker = new PageMaker();
+		//pageMaker. setCri(scri);
+		pageMaker.setTotalCount(service.boardCount(scri));
+		model.addAttribute("pageMaker", pageMaker);
+		
+		return "board/list";	
+	}
+	
+	//게시판 글 보기
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public String read
+	//게시판 수정페이지로 이동
+	
+	
+	
 }
---
+++ web04/src/main/webapp/WEB-INF/views/board/boardRead.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>특정 게시글 보기</title>
+</head>
+<body>
+
+</body>
+</html>
